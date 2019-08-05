f015197d57e6dc2cc12f9eec7e56408d71bba02c
csuhan <1593407377@qq.com>
2019-08-05 17:23:04 +0800

    update router_test

--- router_test.go
+++ router_test.go
@@ -7,9 +7,12 @@ import (
 )
 
 func TestNewRouter(t *testing.T) {
-	mux := New()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello,csuhan")
+	router := New()
+	router.Get("/user", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("CSUHAN GET"))
 	})
-	http.ListenAndServe(":8080", mux)
+	router.HandleFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello,%s", "csuhan")
+	})
+	http.ListenAndServe(":8080", router)
 }
